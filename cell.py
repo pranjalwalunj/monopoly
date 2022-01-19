0c6c1d8f05fc3425bdb2c1b7f1f0abbe29a32421
pranjalwalunj <pranjalwalunj02@gmail.com>
2022-01-19 16:57:52 +0530

    class of cells

--- cell.py
+++ cell.py
@@ -2,8 +2,9 @@ class Cell:
     def __init__(self, name, price, owner):
         self.name = name
         self.price = price
-        self.owner = None
-    def game_rule(self):
-        if self.owner = None
+        self.owner == None
+
+    def get_cell_owner(self):
+        if self.owner == None:
             print("you are on unowned cell")
 
