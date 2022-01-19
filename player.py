ca177d0e2af85d770ca993d7a638e4a8e056c838
pranjalwalunj <pranjalwalunj02@gmail.com>
2022-01-19 16:42:27 +0530

    class of players

--- player.py
+++ player.py
@@ -3,22 +3,24 @@ import random
 
 class Player:
 
-    def __init__(self, name, roll_dice, location, balance, assets):
+    def __init__(self, name, location=0, balance=1000, assets=[]):
         self.name = name
         self.location = location
         self.balance = balance
         self.assets = assets
-        self.roll_dice = roll_dice
 
     def roll_dice(self):
         print("enter 'roll' to roll the die")
-        die = input(" ")
+        roll_dice = input(" ")
         n = random.randint(0, 6)
         print(n)
 
 
 player1 = Player(name='Peter')
 player2 = Player(name='Jack')
+player1.roll_dice()
+player2.roll_dice()
+
 
 
 
