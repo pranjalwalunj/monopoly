958485aec97f120d7f3cfb4125680eb400720bbd
Cyber <cybr1998@gmail.com>
2022-01-20 23:09:06 +0530

    add some utility functions in cell.py

--- cell.py
+++ cell.py
@@ -5,6 +5,17 @@ class Cell:
         self.owner == None
 
     def get_cell_owner(self):
-        if self.owner == None:
-            print("you are on unowned cell")
+        return self.owner
 
+    def get_cell_name(self):
+        return self.name
+
+    def get_cell_price(self):
+        return self.price
+
+    def is_cell_owned(self):
+        if self.owner is None:
+            print('This cell is not owned by anyone.')
+            return None
+        print(f'This cell is owned by {self.owner.name}')
+        return self.owner
