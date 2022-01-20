9217ae07c78e4246b73d495ce045618a601b6000
pranjalwalunj <pranjalwalunj02@gmail.com>
2022-01-20 17:45:46 +0530

    cell details

---
+++ board.py
@@ -0,0 +1,7 @@
+def cell(name, price, owner):
+    pass
+
+
+BOARD = [cell(name='Start', price=0, owner=None),
+         cell(name='Mumbai', price=2350, owner=None),
+         cell(name='Delhi', price=3500, owner=None)]
