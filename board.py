69a242f169e80f96bd1a12b66e928050324b7304
Cyber <cybr1998@gmail.com>
2022-01-21 18:28:24 +0530

    create board

--- board.py
+++ board.py
@@ -1,7 +1,20 @@
-def cell(name, price, owner):
-    pass
+from cell import Cell
 
-
-BOARD = [cell(name='Start', price=0, owner=None),
-         cell(name='Mumbai', price=2350, owner=None),
-         cell(name='Delhi', price=3500, owner=None)]
+BOARD = [
+    Cell(name='Start', price=0, owner=None),
+    Cell(name='Mumbai', price=5000, owner=None),
+    Cell(name='Pune', price=3750, owner=None),
+    Cell(name='Nashik', price=2950, owner=None),
+    Cell(name='Motel', price=-250, owner=None),
+    Cell(name='Delhi', price=4500, owner=None),
+    Cell(name='Gurgaon', price=2500, owner=None),
+    Cell(name='Noida', price=4000, owner=None),
+    Cell(name='Club', price=-250, owner=None),
+    Cell(name='Darjeeling', price=1250, owner=None),
+    Cell(name='Kolkata', price=3850, owner=None),
+    Cell(name='Indore', price=2500, owner=None),
+    Cell(name='Jail', price=-500, owner=None),
+    Cell(name='Chennai', price=3850, owner=None),
+    Cell(name='Community Chest', price=0, owner=None),
+    Cell(name='Hyderabad', price=3500, owner=None),
+]
